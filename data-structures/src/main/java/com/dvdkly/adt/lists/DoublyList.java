46519f74891af63b9664415857f6484f7fda1b06
kellydavid <kellydavid178@gmail.com>
2016-10-30 22:34:25 +0000

    Fixed lots of bugs

--- data-structures/src/main/java/com/dvdkly/adt/lists/DoublyList.java
+++ data-structures/src/main/java/com/dvdkly/adt/lists/DoublyList.java
@@ -7,11 +7,11 @@ public class DoublyList<T> implements List<T> {
     private DNode<T> head;
 
     public int getSize() {
-        if(head == null)
+        if (head == null)
             return 0;
-        int count = 0;
+        int count = 1;
         DNode<T> it = head;
-        while(it.getNext() != null){
+        while (it.getNext() != null) {
             count++;
             it = it.getNext();
         }
@@ -23,11 +23,11 @@ public class DoublyList<T> implements List<T> {
     }
 
     public boolean find(T data) {
-        if(head == null)
+        if (head == null)
             return false;
         DNode<T> it = head;
-        while(it.getNext() != null){
-            if(it.getData().equals(data)){
+        while (it != null) {
+            if (it.getData().equals(data)) {
                 return true;
             }
             it = it.getNext();
@@ -37,9 +37,9 @@ public class DoublyList<T> implements List<T> {
 
     public void insertFirst(T data) {
         DNode<T> newNode = new DNode<>(data);
-        if(head == null)
+        if (head == null)
             head = newNode;
-        else{
+        else {
             head.setPrev(newNode);
             newNode.setNext(head);
             head = newNode;
@@ -48,55 +48,65 @@ public class DoublyList<T> implements List<T> {
 
     public void insertLast(T data) {
         DNode<T> newNode = new DNode<>(data);
-        if(head == null)
+        if (head == null)
             head = newNode;
-        else{
+        else {
             DNode<T> it = head;
-            while(it.getNext() != null){
-                it.setNext(newNode);
-                newNode.setPrev(it);
+            while (it.getNext() != null) {
+                it = it.getNext();
             }
+            it.setNext(newNode);
+            newNode.setPrev(it);
         }
     }
 
     public T get(int index) {
-        if(head == null)
+        if (head == null)
             return null;
         DNode<T> it = head;
         int i = 0;
-        for(; i < index && it.getNext() != null; i++, it = it.getNext()){}
-        if(i == index){
+        for (; i < index && it.getNext() != null; i++, it = it.getNext()) {
+        }
+        if (i == index) {
             return it.getData();
-        }else{
+        } else {
             return null;
         }
     }
 
     public T getFirst() {
-        if(head == null)
+        if (head == null)
             return null;
         else
             return head.getData();
     }
 
     public T getLast() {
-        if(head == null)
+        if (head == null)
             return null;
         DNode<T> it = head;
-        while(it.getNext() != null){
+        while (it.getNext() != null) {
             it = it.getNext();
         }
         return it.getData();
     }
 
     public boolean remove(T data) {
-        if(head == null)
-            return false;
         DNode<T> it = head;
-        while(it.getNext() != null){
-            if(it.getData().equals(data)){
-                it.getPrev().setNext(it.getNext());
-                it.getNext().setPrev(it.getPrev());
+        while (it != null) {
+            if (it.getData().equals(data)) {
+                if (it == head) {
+                    head = head.getNext();
+                } else {
+                    // fix previous pointer
+                    if (it.getPrev() != null) {
+                        it.getPrev().setNext(it.getNext());
+                    }
+                    // fix next pointer
+                    if (it.getNext() != null) {
+                        it.getNext().setPrev(it.getPrev());
+                    }
+                }
                 return true;
             }
             it = it.getNext();
@@ -105,18 +115,27 @@ public class DoublyList<T> implements List<T> {
     }
 
     public boolean remove(int index) {
-        if(head == null)
+        if (head == null)
             return false;
-        DNode<T> it = head;
-        int i = 0;
-        for(; i < index && it.getNext() != null; i++, it = it.getNext()){}
-        if(i == index){
-            it.getPrev().setNext(it.getNext());
-            it.getNext().setPrev(it.getPrev());
+        if (index == 0) {
+            head = head.getNext();
             return true;
-        }else{
-            return false;
         }
+        DNode<T> it = head;
+        for (int i = 0; i <= index && it != null; i++, it = it.getNext()) {
+            if (i == index) {
+                // fix previous pointer
+                if (it.getPrev() != null) {
+                    it.getPrev().setNext(it.getNext());
+                }
+                // fix next pointer
+                if (it.getNext() != null) {
+                    it.getNext().setPrev(it.getPrev());
+                }
+                return true;
+            }
+        }
+        return false;
     }
 
     public void clear() {
@@ -126,11 +145,11 @@ public class DoublyList<T> implements List<T> {
     public String printList() {
         String str = "{";
         DNode<T> ptr = head;
-        while(ptr != null){
+        while (ptr != null) {
             str += ptr.getData() + ", ";
             ptr = ptr.getNext();
         }
-        if(head != null) {
+        if (head != null) {
             str = str.substring(0, str.length() - 2);
         }
         str += "}";
